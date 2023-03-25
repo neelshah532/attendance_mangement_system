5a74446e339ca838133eaeee49a88f1565cd35ac
vaibhavpatel312 <vaibhavkodinariya312@gmail.com>
2023-03-26 00:38:53 +0530

    fetch functionality Done

--- backend/controllers/employeeController.js
+++ backend/controllers/employeeController.js
@@ -5,9 +5,42 @@ const con = connectDb()
 //@method GET
 //@desc Get Employee
 //@PATH /ams/employees/
+const getEmployeesById = asyncHandler(async(req, res) => {
 
+    var getEmployeesById = "SELECT * FROM employees where employeeid=?"
+    const employee = await new Promise((resolve) => {
+        con.query(getEmployeesById, [req.params.id], (err, res) => {
+            if (err)
+                res.send({ success: false, messege: "Something Went Wrong" })
+            resolve(res)
+        })
+    })
+
+    var getSubjectsTakeByEmployee = "SELECT subjects.subjectname FROM subjects INNER JOIN divisions ON divisions.subjectid = subjects.subjectid INNER JOIN employees ON divisions.employeeid = employees.employeeid WHERE employees.employeeid= ?"
+    var getSubjects = await new Promise((resolve) => {
+        con.query(getSubjectsTakeByEmployee, [req.params.id], (err, result) => {
+            if (err)
+                res.send({ success: false, messege: "Something Went Wrong" })
+
+            resolve(result)
+        })
+    })
+    res.send({ success: true, employee: employee, subjects: getSubjects })
+
+})
+
+//@method GET
+//@desc Get Employee
+//@PATH /ams/employees/
 const getAllEmployees = asyncHandler(async(req, res) => {
-    res.send({ messege: "GETTING" })
+    var getAllEmployees = "SELECT employeeid, firstname,middlename,lastname,gender,email FROM employees"
+    con.query(getAllEmployees, (err, result) => {
+        if (err)
+            res.send({ success: false, messege: "Something Went Wrong" })
+
+        res.send({ success: true, employees: result })
+    })
+
 })
 
 
@@ -25,7 +58,6 @@ const takeAttendance = asyncHandler(async(req, res) => {
     const { subject, date, attendance, employeeid } = req.body
 
     if (!subject || !date || !attendance || !employeeid) {
-        console.log("Will Done By Neel")
         res.send(res.json({ success: false, messege: "Please Fill Data" }))
     } else {
         var getColumnIFExsist = `SHOW COLUMNS FROM ${subject}`
@@ -78,6 +110,7 @@ const responseQueryToStudent = asyncHandler(async(req, res) => {
 module.exports = {
     employeeLogin,
     takeAttendance,
+    getEmployeesById,
     getAllEmployees,
     updateStudentAttendace,
     responseQueryToStudent
--- backend/routes/employeeRoutes.js
+++ backend/routes/employeeRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router()
-const { employeeLogin, takeAttendance, getAllEmployees, responseQueryToStudent, updateStudentAttendace } = require('../controllers/employeeController')
+const { employeeLogin, getEmployeesById, takeAttendance, getAllEmployees, responseQueryToStudent, updateStudentAttendace } = require('../controllers/employeeController')
 
 router.get('/', getAllEmployees)
+router.get('/:id', getEmployeesById)
 router.post('/login', employeeLogin)
 router.route('/attendance').post(takeAttendance).put(updateStudentAttendace)
 router.post('/query', responseQueryToStudent)
