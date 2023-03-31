9966e0f2c6dc56af5a51817f2a33f6ffbf6543f9
vaibhavpatel312 <vaibhavkodinariya312@gmail.com>
2023-03-31 16:09:22 +0530

    response changes

--- backend/controllers/studentsController.js
+++ backend/controllers/studentsController.js
@@ -7,56 +7,55 @@ const con = connectDb()
 //@PATH /ams/students/
 
 
-const studentData = asyncHandler(async (req, res) => {
+const studentData = asyncHandler(async(req, res) => {
     // Query MySQL for student data
     const query = `SELECT enrollmentno,firstname,middlename,lastname FROM students`;
-    
-      con.query(query,(err,result)=>{
-        if(err){
-           res.send({success:false,messege:"Something Went Wrong"})
-        }else{
-            res.status(200).json({success:true,students:result})
-          }
-      })
-  });
+    con.query(query, (err, result) => {
+        if (err) {
+            res.send({ success: false, messege: "Something Went Wrong" })
+        } else {
+            res.status(200).json({ success: true, students: result })
+        }
+    })
+});
 
 const getStudentsById = asyncHandler(async(req, res) => {
     const studentsId = req.params.id;
-    if(!studentsId){
-      res.send({ success: false, messege: "Something Went Wrong" })
-    }else{
-      const getStudentsById = "SELECT * FROM students where enrollmentno=?";
-          try {
-              con.query(getStudentsById, [studentsId],(err,result)=>{
+    if (!studentsId) {
+        res.send({ success: false, messege: "Something Went Wrong" })
+    } else {
+        const getStudentsById = "SELECT * FROM students where enrollmentno=?";
+        try {
+            con.query(getStudentsById, [studentsId], (err, result) => {
                 res.send({ success: true, students: result })
-              });
-            } catch (error) { 
-              res.send({ success: false, messege: "Something Went Wrong" })
-            }
+            });
+        } catch (error) {
+            res.send({ success: false, messege: "Something Went Wrong" })
+        }
     }
 });
 
 
-  module.exports = {
-    studentData,
-    getStudentsById
-}
-  // const express = require('express')
-// const mysql= require('mysql')
-// const app = express()
-// const con = mysql.createConnection({
-//     host: 'localhost',
-//     user: 'root',
-//     password: '',
-//     database: 'attendance_management_system'
-// })
-// con.connect((err) => {
-//     if(err){
-//         console.log(err)
-//     } else{
-//         console.log("Connected")
-//     }
-// })
+module.exports = {
+        studentData,
+        getStudentsById
+    }
+    // const express = require('express')
+    // const mysql= require('mysql')
+    // const app = express()
+    // const con = mysql.createConnection({
+    //     host: 'localhost',
+    //     user: 'root',
+    //     password: '',
+    //     database: 'attendance_management_system'
+    // })
+    // con.connect((err) => {
+    //     if(err){
+    //         console.log(err)
+    //     } else{
+    //         console.log("Connected")
+    //     }
+    // })
 
 // app.get("/fetch",(req,res) => {
 //     con.query("SELECT * FROM students",function(err,result){
--- backend/routes/studentsRoutes.js
+++ backend/routes/studentsRoutes.js
@@ -1,13 +1,8 @@
-
 const express = require('express')
 const router = express.Router()
-const { studentData, getStudentsById    } = require('../controllers/studentsController')
+const { studentData, getStudentsById } = require('../controllers/studentsController')
 
 router.get('/', studentData)
 router.get('/:id', getStudentsById)
 
 module.exports = router
-
-
-
-  
