3c5b771f54e44776380854366b8917da81f5c647
vaibhavpatel312 <vaibhavkodinariya312@gmail.com>
2023-04-02 17:15:50 +0530

    file name chages

--- backend/controllers/studentsController.js
+++ backend/controllers/studentsController.js
@@ -3,7 +3,7 @@ const { connectDb } = require('../config/db')
 const con = connectDb()
 
 //@method GET
-//@desc Get Employee
+//@desc Get Students
 //@PATH /ams/students/
 const studentData = asyncHandler(async(req, res) => {
     // Query MySQL for student data
@@ -19,7 +19,7 @@ const studentData = asyncHandler(async(req, res) => {
 
 
 //@method GET
-//@desc Get Employee
+//@desc Get Students
 //@PATH /ams/students/:id
 const getStudentsById = asyncHandler(async(req, res) => {
     const studentsId = req.params.id;
