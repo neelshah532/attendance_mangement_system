f59e70611f78b48dadbdbb5d0e811804468fd4d4
vaibhavpatel312 <vaibhavkodinariya312@gmail.com>
2023-04-09 12:58:36 +0530

    removing all files

--- backend/routes/loginRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const { login} = require('../controllers/studentsController')
-
-router.post('/login', login)
-
-module.exports = router
