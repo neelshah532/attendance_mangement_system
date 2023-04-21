605b02bc471b704df02fa06bcab2952713aae0b3
vaibhavpatel312 <vaibhavkodinariya312@gmail.com>
2023-04-21 11:43:56 +0530

    routes Done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useLayoutEffect, useState } from "react";
 import { ChakraProvider } from "@chakra-ui/react";
 import Login from "./pages/Login";
 import Pages from "./components/Pages";
-import { useNavigate } from "react-router-dom";
 
 function App() {
   const [isLoggedIn,setIsLoggedIn] = useState(false);
--- frontend/src/components/Manage.jsx
+++ frontend/src/components/Manage.jsx
@@ -6,8 +6,10 @@ import icon1 from "../images/Group 55.png";
 import icon2 from "../images/Group 58.png";
 import icon3 from "../images/Group 54.png";
 import icon4 from "../images/Group 59.png";
+import { useNavigate } from "react-router-dom";
 
 function Manage() {
+  const navigate=useNavigate()
   return (
     <Box bg="#1A237E" h="100vh" w="206vh" overflow="hidden">
         <Image src={background} alt="Logo" w="691dp" h="100vh" mx="auto" mt="5dp"  opacity={0.5}/>
@@ -40,7 +42,7 @@ function Manage() {
           left="50%"
           transform="translate(-50%, -50%)"
         >
-          <Box bg="#D9D9D9" w="226px" h="170px" borderRadius="10px" >
+          <Box bg="#D9D9D9" w="226px" h="170px" borderRadius="10px" onClick={()=>{navigate('/AddTeacher')}}>
             <Image src={icon1} alt="Icon 1" w="100px" h="100px" mx="auto" mt="35px" />
             <Text 
             ml="2"
@@ -52,7 +54,7 @@ function Manage() {
             > 
             Add Teaching Staff</Text>
           </Box>
-          <Box bg="#D9D9D9" w="226px" h="170px" borderRadius="10px">
+          <Box bg="#D9D9D9" w="226px" h="170px" borderRadius="10px" onClick={()=>{navigate('/ViewTeacher')}}>
             <Image src={icon2} alt="Icon 2" w="100px" h="100px" mx="auto" mt="35px" />
             <Text 
             ml="2"
@@ -64,7 +66,7 @@ function Manage() {
             > 
             View Teaching Staff</Text>
           </Box>
-          <Box bg="#D9D9D9" w="226px" h="170px" borderRadius="10px">
+          <Box bg="#D9D9D9" w="226px" h="170px" borderRadius="10px" onClick={()=>{navigate('/AddStudent')}}>
             <Image src={icon3} alt="Icon 3" w="100px" h="100px" mx="auto" mt="35px" />
             <Text 
             ml="2"
@@ -77,7 +79,7 @@ function Manage() {
             Add Student
             </Text>
           </Box>
-          <Box bg="#D9D9D9" w="226px" h="170px" borderRadius="10px">
+          <Box bg="#D9D9D9" w="226px" h="170px" borderRadius="10px" onClick={()=>{navigate('/ViewStudent')}}>
             <Image src={icon4} alt="Icon 4" w="100px" h="100px" mx="auto" mt="35px" />
             <Text 
             ml="2"
@@ -85,8 +87,7 @@ function Manage() {
             color="#1A237E"
             fontSize="md"
             fontFamily={"noto-serif"}
-            textAlign="center"
-            > 
+            textAlign="center"> 
             View Student</Text>
           </Box>
         </Grid> 
